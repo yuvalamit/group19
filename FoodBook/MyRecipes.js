cec8789bdd2033f163306fbf59bb2d4522943277
yuvalamit <yuami@post.bgu.ac.il>
2024-02-19 00:39:32 +0200

    changes 18/02

---
+++ FoodBook/MyRecipes.js
@@ -0,0 +1,45 @@
+window.onload = function () {
+  showHelloLabel();
+  loadRecipes();
+};
+
+function showHelloLabel() {
+  const helloLabel = document.querySelector("#helloUser");
+  const firstName = usersList[0].firstName;
+  helloLabel.textContent = `שלום, ${firstName}`;
+}
+
+const loadRecipes = () => {
+  const recipes = usersList[0].recipes;
+  recipes.forEach((recipe) => {
+    createRecipeCube(recipe);
+  });
+};
+
+const createRecipeCube = (recipe) => {
+  const recipeDiv = document.createElement("div");
+  recipeDiv.classList.add("recipe");
+
+  const heading = document.createElement("h3");
+  heading.textContent = recipe.name;
+
+  const imageLink = document.createElement("a");
+  imageLink.href = "Recipe.html";
+
+  const image = document.createElement("img");
+  image.src = recipe.image;
+  image.alt = recipe.name;
+  image.height = "250";
+
+  imageLink.appendChild(image);
+
+  const duration = document.createElement("h4");
+  duration.textContent = `${recipe.hardness} | ${recipe.minutes} דקות`;
+
+  recipeDiv.appendChild(heading);
+  recipeDiv.appendChild(imageLink);
+  recipeDiv.appendChild(duration);
+
+  const allRecipes = document.getElementById("AllRecipes");
+  allRecipes.appendChild(recipeDiv);
+};
