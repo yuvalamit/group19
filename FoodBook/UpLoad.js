d7fac43393e8abe8420c51c17d975713b3282034
adilouk <alouk0545577049@gmail.com>
2024-02-20 12:08:25 +0200

    done, without DB

--- FoodBook/UpLoad.js
+++ FoodBook/UpLoad.js
@@ -1,16 +1,23 @@
+window.onload = function () {
 const form = document.querySelector("form");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-
-  const name = document.getElementById("NAME").value;
-
-  const recipes = usersList[0].recipes;
-  recipes.forEach((recipe) => {
-    if (recipe.name === name) {
+  
+  if(checkRecipeExist(form.elements["nameRec"].value)){
       alert("קיים מתכון עם שם זהה. לא נוצר המתכון!");
     } else {
       alert("מתכון הוסף בהצלחה!");
     }
-  });
 });
+
+const checkRecipeExist = (name) => {
+  const nameRecipesList = usersList[0].recipes.map((recipe) => recipe.name)
+  for(let Index in nameRecipesList){
+    const nameRecipe = nameRecipesList[Index];
+    if(nameRecipe === name)
+      return true;
+  }
+  return false;
+}
+}
